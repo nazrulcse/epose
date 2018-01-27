69f0908f251ca2f720fb6ce1c0d9fb2ff8a54041
Nazrul <nazrulku07@gmail.com>
2018-01-27 11:21:21 +0600

    remove some packages

--- EPose/Layout.Designer.cs
+++ EPose/Layout.Designer.cs
@@ -30,18 +30,21 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Layout));
             this.titleBar = new System.Windows.Forms.Panel();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.btnexit = new System.Windows.Forms.PictureBox();
             this.formTitle = new System.Windows.Forms.Label();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.frameIcon = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.frameLeftBorder = new System.Windows.Forms.Panel();
+            this.frameRightBorder = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.titleBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnexit)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.frameIcon)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // titleBar
@@ -51,21 +54,34 @@
             this.titleBar.Controls.Add(this.btnexit);
             this.titleBar.Controls.Add(this.formTitle);
             this.titleBar.Controls.Add(this.pictureBox3);
-            this.titleBar.Controls.Add(this.pictureBox1);
+            this.titleBar.Controls.Add(this.frameIcon);
             this.titleBar.Controls.Add(this.pictureBox2);
             this.titleBar.Dock = System.Windows.Forms.DockStyle.Top;
             this.titleBar.Location = new System.Drawing.Point(0, 0);
             this.titleBar.Name = "titleBar";
-            this.titleBar.Size = new System.Drawing.Size(780, 40);
+            this.titleBar.Size = new System.Drawing.Size(788, 40);
             this.titleBar.TabIndex = 2;
             this.titleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.titleBar_MouseDown);
             this.titleBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.titleBar_MouseMove);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(621, 0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Padding = new System.Windows.Forms.Padding(0, 0, 5, 0);
+            this.pictureBox4.Size = new System.Drawing.Size(45, 40);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
             // 
             // btnexit
             // 
             this.btnexit.Dock = System.Windows.Forms.DockStyle.Right;
             this.btnexit.Image = ((System.Drawing.Image)(resources.GetObject("btnexit.Image")));
-            this.btnexit.Location = new System.Drawing.Point(658, 0);
+            this.btnexit.Location = new System.Drawing.Point(666, 0);
             this.btnexit.Name = "btnexit";
             this.btnexit.Padding = new System.Windows.Forms.Padding(0, 0, 5, 0);
             this.btnexit.Size = new System.Drawing.Size(45, 40);
@@ -92,7 +108,7 @@
             // 
             this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Right;
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(703, 0);
+            this.pictureBox3.Location = new System.Drawing.Point(711, 0);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Padding = new System.Windows.Forms.Padding(0, 0, 5, 0);
             this.pictureBox3.Size = new System.Drawing.Size(35, 40);
@@ -100,21 +116,21 @@
             this.pictureBox3.TabIndex = 3;
             this.pictureBox3.TabStop = false;
             // 
-            // pictureBox1
+            // frameIcon
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(4, 4);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(29, 30);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
+            this.frameIcon.Image = ((System.Drawing.Image)(resources.GetObject("frameIcon.Image")));
+            this.frameIcon.Location = new System.Drawing.Point(4, 4);
+            this.frameIcon.Name = "frameIcon";
+            this.frameIcon.Size = new System.Drawing.Size(29, 30);
+            this.frameIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.frameIcon.TabIndex = 3;
+            this.frameIcon.TabStop = false;
             // 
             // pictureBox2
             // 
             this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Right;
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(738, 0);
+            this.pictureBox2.Location = new System.Drawing.Point(746, 0);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Padding = new System.Windows.Forms.Padding(0, 0, 15, 0);
             this.pictureBox2.Size = new System.Drawing.Size(42, 40);
@@ -122,35 +138,52 @@
             this.pictureBox2.TabIndex = 3;
             this.pictureBox2.TabStop = false;
             // 
-            // pictureBox4
+            // frameLeftBorder
             // 
-            this.pictureBox4.Dock = System.Windows.Forms.DockStyle.Right;
-            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-            this.pictureBox4.Location = new System.Drawing.Point(613, 0);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Padding = new System.Windows.Forms.Padding(0, 0, 5, 0);
-            this.pictureBox4.Size = new System.Drawing.Size(45, 40);
-            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox4.TabIndex = 4;
-            this.pictureBox4.TabStop = false;
-            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            this.frameLeftBorder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(165)))), ((int)(((byte)(223)))));
+            this.frameLeftBorder.Dock = System.Windows.Forms.DockStyle.Left;
+            this.frameLeftBorder.Location = new System.Drawing.Point(0, 40);
+            this.frameLeftBorder.Name = "frameLeftBorder";
+            this.frameLeftBorder.Size = new System.Drawing.Size(3, 422);
+            this.frameLeftBorder.TabIndex = 3;
+            // 
+            // frameRightBorder
+            // 
+            this.frameRightBorder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(165)))), ((int)(((byte)(223)))));
+            this.frameRightBorder.Dock = System.Windows.Forms.DockStyle.Right;
+            this.frameRightBorder.Location = new System.Drawing.Point(785, 40);
+            this.frameRightBorder.Name = "frameRightBorder";
+            this.frameRightBorder.Size = new System.Drawing.Size(3, 422);
+            this.frameRightBorder.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(165)))), ((int)(((byte)(223)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(3, 459);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(782, 3);
+            this.panel1.TabIndex = 5;
             // 
             // Layout
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(780, 463);
+            this.ClientSize = new System.Drawing.Size(788, 462);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.frameRightBorder);
+            this.Controls.Add(this.frameLeftBorder);
             this.Controls.Add(this.titleBar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Layout";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Layout";
             this.titleBar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnexit)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.frameIcon)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -158,12 +191,15 @@
         #endregion
 
         private System.Windows.Forms.Panel titleBar;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox frameIcon;
         private System.Windows.Forms.Label formTitle;
         private System.Windows.Forms.PictureBox btnexit;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Panel frameLeftBorder;
+        private System.Windows.Forms.Panel frameRightBorder;
+        private System.Windows.Forms.Panel panel1;
 
     }
 }
