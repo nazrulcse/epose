03f7c3abfc756779c9bca8f39c4e3b920315a565
Nazrul <nazrulku07@gmail.com>
2018-01-27 10:39:07 +0600

    missing file

---
+++ EPose/Department_Frame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EPose
+{
+    public partial class Department_Frame : Layout
+    {
+        public Department_Frame()
+        {
+            InitializeComponent();
+        }
+
+        private void Department_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
