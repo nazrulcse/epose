f8dc54fcf3a64f7795a86ed335700606280cf97d
ahadhossainapon <ahadhossain73@gmail.com>
2018-01-31 18:08:30 +0600

    add payment Model

--- EPose/payment_Frame.Designer.cs
+++ EPose/payment_Frame.Designer.cs
@@ -34,47 +34,57 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.customerList = new System.Windows.Forms.DataGridView();
+            this.paymentList = new System.Windows.Forms.DataGridView();
             this.EName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Designation = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Email = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Phone = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Joining = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.customerList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paymentList)).BeginInit();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.customerList);
-            this.panel1.Location = new System.Drawing.Point(3, 222);
+            this.panel1.Controls.Add(this.paymentList);
+            this.panel1.Location = new System.Drawing.Point(7, 268);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(879, 322);
+            this.panel1.Size = new System.Drawing.Size(1169, 402);
             this.panel1.TabIndex = 4;
             // 
-            // customerList
+            // paymentList
             // 
-            this.customerList.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
-            this.customerList.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
-            this.customerList.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
-            this.customerList.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.customerList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.customerList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.paymentList.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.paymentList.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.paymentList.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.paymentList.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.paymentList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.paymentList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.EName,
             this.Designation,
             this.Email,
             this.Phone,
             this.Joining,
-            this.Column1});
-            this.customerList.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.customerList.GridColor = System.Drawing.SystemColors.ActiveCaption;
-            this.customerList.Location = new System.Drawing.Point(0, 0);
-            this.customerList.Name = "customerList";
-            this.customerList.RowHeadersWidth = 52;
-            this.customerList.Size = new System.Drawing.Size(879, 322);
-            this.customerList.TabIndex = 4;
+            this.Column1,
+            this.Column2});
+            this.paymentList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.paymentList.GridColor = System.Drawing.SystemColors.ActiveCaption;
+            this.paymentList.Location = new System.Drawing.Point(0, 0);
+            this.paymentList.Margin = new System.Windows.Forms.Padding(4);
+            this.paymentList.Name = "paymentList";
+            this.paymentList.RowHeadersWidth = 52;
+            this.paymentList.Size = new System.Drawing.Size(1169, 402);
+            this.paymentList.TabIndex = 8;
             // 
             // EName
             // 
@@ -119,28 +129,90 @@
             this.Joining.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.Joining.DefaultCellStyle = dataGridViewCellStyle5;
-            this.Joining.HeaderText = "Banlance";
+            this.Joining.HeaderText = "Amount";
             this.Joining.Name = "Joining";
             // 
             // Column1
             // 
             dataGridViewCellStyle6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.Column1.DefaultCellStyle = dataGridViewCellStyle6;
-            this.Column1.HeaderText = "Date";
+            this.Column1.HeaderText = "transaction token";
             this.Column1.Name = "Column1";
-            this.Column1.Width = 55;
+            this.Column1.Width = 130;
+            // 
+            // Column2
+            // 
+            dataGridViewCellStyle7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.Column2.DefaultCellStyle = dataGridViewCellStyle7;
+            this.Column2.HeaderText = "Date";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 63;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.comboBox1);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Location = new System.Drawing.Point(32, 91);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(694, 153);
+            this.panel2.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Bikash",
+            "Cash",
+            "Card",
+            "Rocket"});
+            this.comboBox1.Location = new System.Drawing.Point(238, 34);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(39, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(183, 28);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Select payment Type";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(238, 81);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(39, 75);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(183, 28);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "Enter Amount";
             // 
             // payment_Frame
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(885, 547);
+            this.ClientSize = new System.Drawing.Size(1180, 673);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.Name = "payment_Frame";
             this.Text = "payment_Frame";
             this.Controls.SetChildIndex(this.panel1, 0);
+            this.Controls.SetChildIndex(this.panel2, 0);
             this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.customerList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paymentList)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -148,12 +220,19 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.DataGridView customerList;
+        private System.Windows.Forms.DataGridView paymentList;
         private System.Windows.Forms.DataGridViewTextBoxColumn EName;
         private System.Windows.Forms.DataGridViewTextBoxColumn Designation;
         private System.Windows.Forms.DataGridViewTextBoxColumn Email;
         private System.Windows.Forms.DataGridViewTextBoxColumn Phone;
         private System.Windows.Forms.DataGridViewTextBoxColumn Joining;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+
     }
 }
--- EPose/payment_Frame.cs
+++ EPose/payment_Frame.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EPose.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,6 +17,20 @@ namespace EPose
         {
             InitializeComponent();
             this.setTitle("Payment Window");
+            
+            PaymentModel payment = new PaymentModel();
+            payment.global_id = "111";
+            payment.payment_type = "bikash";
+            payment.invoice_id = "122";
+            payment.amount = 23.4;
+            payment.transaction_token = "7070";
+            payment.create(payment);
+
+            dynamic payments = payment.all(payment);
+            foreach(var pay in payments){
+                paymentList.Rows.Add(pay.global_id, pay.payment_type, pay.invoice_id, pay.amount, pay.transaction_token, pay.transaction_token);
+            }
+
         }
     }
 }
